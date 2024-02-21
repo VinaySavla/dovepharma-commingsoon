f54db376fb22e1bb69ac15209a8e5abbd5a0c8d7
VinaySavla <savlavinay77@gmail.com>
2024-02-21 19:38:30 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <br />
+      <div className="card">
+        <div className="content">
+          <div className="title-holder">
+            <img src={logo} className="logo" height={150} alt="logo" />
+            <h1>Get ready for the change.</h1>
+            <p>Website coming soon. Please check back to know more.</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
